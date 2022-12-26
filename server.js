9bf27fa775b6ef2658662640e07d4f32510d5e3b
Syahruzein <syahruzein@example.com>
2022-12-26 19:20:48 +0700

    versi revisi sidang PA2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const rapatRoutes = require('./app/routes/meet.routes');
 const notulenRoutes = require('./app/routes/notulen.routes');
-var whitelist = ['http://pjj2022-syahru.s3-website.ap-southeast-3.amazonaws.com/', 'http://localhost:8200']
+var whitelist = ['http://pjj2022-syahru.s3-website.ap-southeast-3.amazonaws.com/', 'https://apr-pens.netlify.app/', 'http://localhost:8200']
 
 // var corsOptions = {
 //     origin: "http://localhost:8200"
